c1983a6af91a82f3fafd312ced48bd62346e4109
Om Gupta <omgupta02005@gmail.com>
2025-11-23 19:41:19 +0530

    Create Instructor.java

---
+++ edu/ccrm/domain/Instructor.java
@@ -0,0 +1,23 @@
+package edu.ccrm.domain;
+
+public class Instructor extends Person {
+    private String department;
+
+    public Instructor(int id, String fullName, String email, String department) {
+        super(id, fullName, email);
+        this.department = department;
+    }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+
+    @Override
+    public String getProfile() {
+        return "Instructor Profile: " + super.toString() + ", Department: " + department;
+    }
+
+    @Override
+    public String toString() {
+        return "[Instructor] " + getProfile();
+    }
+}
